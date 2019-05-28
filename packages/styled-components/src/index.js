0f0204f56ced6f22a968a6e993864289cdc9d6e1
Greg Bergé <greg@smooth-code.com>
2019-05-28 14:16:42 +0200

    fix: explicit exports from styled-components (#5)
    Fix the usage in CommonJS world (also on CodeSandbox)
    
    Closes #4

--- packages/styled-components/src/index.js
+++ packages/styled-components/src/index.js
@@ -1,4 +1,18 @@
-export * from 'styled-components'
+export {
+  createGlobalStyle,
+  isStyledComponent,
+  keyframes,
+  ServerStyleSheet,
+  StyleSheetConsumer,
+  StyleSheetContext,
+  StyleSheetManager,
+  ThemeConsumer,
+  ThemeContext,
+  ThemeProvider,
+  withTheme,
+  __DO_NOT_USE_OR_YOU_WILL_BE_HAUNTED_BY_SPOOKY_GHOSTS,
+} from 'styled-components'
+
 export { css } from './css'
 export { Box } from './Box'
 export { styled as default } from './styled'
