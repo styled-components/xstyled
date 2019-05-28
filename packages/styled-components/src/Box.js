cb62872f4d17e159afb61b96a688fb58c3e5c76e
Greg Bergé <greg@smooth-code.com>
2019-05-28 22:02:58 +0200

    fix: increase box specificity

--- packages/styled-components/src/Box.js
+++ packages/styled-components/src/Box.js
@@ -1,4 +1,8 @@
 import styled from 'styled-components'
 import { system } from '@xstyled/system'
 
-export const Box = styled.div(system)
+export const Box = styled.div`
+  && {
+    ${system}
+  }
+`
