2658a078813608780e153a18b4205fbc2f2b73b5
Greg Bergé <greg@smooth-code.com>
2019-07-09 14:33:00 +0200

    fix: fix rpxTransformer => rpxTransformers

--- packages/system/src/transformers.js
+++ packages/system/src/transformers.js
@@ -1,7 +1,7 @@
 import { num } from './util'
 import { remPx, rpx } from './unit'
 
-export const rpxTransformer = {
+export const rpxTransformers = {
   px: value => remPx(rpx(value)),
   border: n => (num(n) && n > 0 ? `${remPx(n)} solid` : n),
 }
--- packages/system/src/transformers.test.js
+++ packages/system/src/transformers.test.js
@@ -1,13 +1,13 @@
 import { th } from './th'
-import { rpxTransformer } from './transformers'
+import { rpxTransformers } from './transformers'
 
-describe('#rpxTransformer', () => {
+describe('#rpxTransformers', () => {
   it('should use rem instead of px', () => {
     const baseProps = { theme: { transformers: {} } }
     expect(th.space(1)(baseProps)).toBe('4px')
     expect(th.space(2)(baseProps)).toBe('8px')
     expect(th.border(2)(baseProps)).toBe('2px solid')
-    const remProps = { theme: { transformers: { ...rpxTransformer } } }
+    const remProps = { theme: { transformers: { ...rpxTransformers } } }
     expect(th.space(1)(remProps)).toBe('0.25rem')
     expect(th.space(2)(remProps)).toBe('0.5rem')
     expect(th.border(2)(remProps)).toBe('0.125rem solid')
