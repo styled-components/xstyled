f12e2ca5bf059fb97e2e23307c48b1f15ede12d2
Greg Bergé <greg@smooth-code.com>
2019-10-02 18:53:32 +0200

    docs: add algolia docsearch

--- website/gatsby-config.js
+++ website/gatsby-config.js
@@ -13,6 +13,10 @@ module.exports = {
         menu: ['About', 'Introduction', 'Guides', 'API'],
         nav: [{ title: 'Docs', url: '/docs/' }],
         codeFundProperty: 306,
+        algoliaDocSearch: {
+          apiKey: '9a5b8a758fdc63c340972ae48583c2f9',
+          indexName: 'smooth-code-xstyled',
+        },
       },
     },
   ],
