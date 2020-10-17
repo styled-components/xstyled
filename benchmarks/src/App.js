78632620a6aee94b4b53a9529e23bf2133b70fea
Jan Czizikow <jan.czizikow@gmail.com>
2020-10-17 23:26:49 +0200

    chore: added emotion benchmark

--- benchmarks/src/App.js
+++ benchmarks/src/App.js
@@ -2,6 +2,8 @@ import React from 'react'
 import BigTable from './components/BigTable'
 // import Table from './components/ReactTable'
 // import Table from './components/SCTable'
+// import Table from './components/EmotionTable'
+// import Table from './components/XStyledEmotionTable'
 import Table from './components/XStyledTable'
 
 export default () => <BigTable Table={Table} />
