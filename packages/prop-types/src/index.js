160cffc9be002a54da130a45061e72f531a4c261
Ivan Banov <ivanbanov@gmail.com>
2020-10-30 11:08:18 +0100

    Add bool option to the prop-types to use it with col and row props

--- packages/prop-types/src/index.js
+++ packages/prop-types/src/index.js
@@ -1,9 +1,9 @@
-import { oneOfType, number, string, object } from 'prop-types'
+import { oneOfType, number, string, object, bool } from 'prop-types'
 
 export function getSystemPropTypes(system) {
   if (!system) return {}
   return system.meta.props.reduce((obj, prop) => {
-    obj[prop] = oneOfType([number, string, object])
+    obj[prop] = oneOfType([number, string, object, bool])
     return obj
   }, {})
 }
