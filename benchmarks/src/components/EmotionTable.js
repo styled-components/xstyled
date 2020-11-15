3bb271853495da91a1d4e4fe335a9f7f1622a407
Jan Czizikow <jan.czizikow@gmail.com>
2020-11-15 17:36:25 +0100

    chore: added emotion benchmark (#139)

---
+++ benchmarks/src/components/EmotionTable.js
@@ -0,0 +1,34 @@
+import React from 'react'
+import styled from '@emotion/styled'
+
+const Table = styled.div`
+  display: table;
+  margin-top: 10px;
+`
+
+const Row = styled.div`
+  display: table-row;
+`
+
+const Cell = styled.div`
+  display: table-cell;
+  padding: 10px;
+  background: rgba(74, 174, 53, ${(props) => props.value});
+`
+
+export default ({ table, toPercent }) => (
+  <>
+    <h3>emotion</h3>
+    <Table>
+      {table.map((row, i) => (
+        <Row key={i}>
+          {row.map((x, j) => (
+            <Cell key={`${i}${j}`} value={x}>
+              {toPercent(x)}
+            </Cell>
+          ))}
+        </Row>
+      ))}
+    </Table>
+  </>
+)
