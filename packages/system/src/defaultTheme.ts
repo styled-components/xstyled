bed13602cd63a32d303ef348c54611b1e796af4a
Greg Bergé <greg@smooth-code.com>
2020-11-29 10:22:27 +0100

    chore(defaultTheme): fix typo

--- packages/system/src/defaultTheme.ts
+++ packages/system/src/defaultTheme.ts
@@ -442,6 +442,6 @@ export const defaultTheme = {
     spin: 'x-spin 1s linear infinite',
     ping: 'x-ping 1s cubic-bezier(0, 0, 0.2, 1) infinite',
     pulse: 'x-pulse 2s cubic-bezier(0.4, 0, 0.6, 1) infinite',
-    bounde: 'x-bounce 1s infinite',
+    bounce: 'x-bounce 1s infinite',
   },
 }
