be120c81c84183ef67de7047009f83def4ad6099
Greg Bergé <greg@smooth-code.com>
2020-12-08 23:15:38 +0100

    feat: add boxSizing & container

--- packages/system/src/styles/layout.ts
+++ packages/system/src/styles/layout.ts
@@ -1,5 +1,12 @@
 import * as CSS from 'csstype'
-import { style, compose } from '../style'
+import { obj } from '@xstyled/util'
+import {
+  style,
+  compose,
+  createStyleGenerator,
+  reduceBreakpoints,
+} from '../style'
+import { getBreakpoints } from '../media'
 import { SystemProperty } from '../types'
 
 export interface DisplayProps<T = {}> {
@@ -8,6 +15,38 @@ export interface DisplayProps<T = {}> {
 export const display = style<DisplayProps>({
   prop: 'display',
 })
+
+export interface BoxSizingProps<T = {}> {
+  boxSizing?: SystemProperty<CSS.Property.BoxSizing, T>
+}
+export const boxSizing = style<BoxSizingProps>({
+  prop: 'boxSizing',
+})
+
+export interface ContainerProps<T = {}> {
+  container?: SystemProperty<boolean, T>
+}
+export const container = createStyleGenerator<ContainerProps>(
+  (props) => {
+    if (!props.container) return null
+    const breakpoints = getBreakpoints(props)
+    let styles = reduceBreakpoints(
+      props,
+      breakpoints,
+      (breakpointValue: string | number) =>
+        breakpointValue !== 0 ? { maxWidth: breakpointValue } : {},
+    )
+    if (obj(props.container)) {
+      styles = reduceBreakpoints(props, props.container, () => styles)
+    }
+
+    return {
+      width: '100%',
+      ...styles,
+    }
+  },
+  ['container'],
+)
 
 export interface OverflowProps<T = {}> {
   overflow?: SystemProperty<CSS.Property.Overflow, T>
@@ -31,11 +70,15 @@ export const overflowY = style<OverflowYProps>({
 })
 
 export type LayoutProps<T = {}> = DisplayProps<T> &
+  BoxSizingProps<T> &
+  ContainerProps<T> &
   OverflowProps<T> &
   OverflowXProps<T> &
   OverflowYProps<T>
 export const layout = compose<LayoutProps>(
+  boxSizing,
   display,
+  container,
   overflow,
   overflowX,
   overflowY,
