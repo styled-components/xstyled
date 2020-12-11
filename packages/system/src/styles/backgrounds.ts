a317cc170608c8e1629cd2aa070935b685890d7e
Greg Bergé <greg@smooth-code.com>
2020-12-11 14:37:21 +0100

    feat: add `gradientFrom`, `gradientVia`, `gradientTo` utilities

--- packages/system/src/styles/backgrounds.ts
+++ packages/system/src/styles/backgrounds.ts
@@ -3,11 +3,25 @@ import { style, compose } from '../style'
 import { getColor, ColorGetter } from './colors'
 import { SystemProperty } from '../types'
 
+const gradientBackgrounds: { [key: string]: string } = {
+  'gradient-to-t': 'linear-gradient(to top, var(--x-gradient-stops))',
+  'gradient-to-tr': 'linear-gradient(to top right, var(--x-gradient-stops))',
+  'gradient-to-r': 'linear-gradient(to right, var(--x-gradient-stops))',
+  'gradient-to-br': 'linear-gradient(to bottom right, var(--x-gradient-stops))',
+  'gradient-to-b': 'linear-gradient(to bottom, var(--x-gradient-stops))',
+  'gradient-to-bl': 'linear-gradient(to bottom left, var(--x-gradient-stops))',
+  'gradient-to-l': 'linear-gradient(to left, var(--x-gradient-stops))',
+  'gradient-to-tl': 'linear-gradient(to top left, var(--x-gradient-stops))',
+}
+
 export interface BackgroundProps<T = {}> {
   background?: SystemProperty<CSS.Property.Background, T>
 }
 export const background = style<BackgroundProps>({
   prop: 'background',
+  cssProperty: (_, { value }) => ({
+    background: gradientBackgrounds[value] || value,
+  }),
 })
 
 type BackgroundColor<T = {}> = ColorGetter<T> | CSS.Property.BackgroundColor
@@ -26,6 +40,9 @@ export interface BackgroundImageProps<T = {}> {
 }
 export const backgroundImage = style<BackgroundImageProps>({
   prop: 'backgroundImage',
+  cssProperty: (_, { value }) => ({
+    background: gradientBackgrounds[value] || value,
+  }),
 })
 
 export interface BackgroundSizeProps<T = {}> {
@@ -64,6 +81,41 @@ export const backgroundClip = style<BackgroundClipProps>({
   cssProperty: ['backgroundClip', '-webkitBackgroundClip'],
 })
 
+export interface GradientFromProps<T = {}> {
+  gradientFrom?: SystemProperty<ColorGetter, T>
+}
+export const gradientFrom = style<GradientFromProps>({
+  prop: 'gradientFrom',
+  themeGet: getColor,
+  cssProperty: (_, { value }) => {
+    return {
+      '--x-gradient-from': value,
+      '--x-gradient-stops':
+        'var(--x-gradient-from), var(--x-gradient-to, transparent)',
+    }
+  },
+})
+
+export interface GradientViaProps<T = {}> {
+  gradientVia?: SystemProperty<ColorGetter, T>
+}
+export const gradientVia = style<GradientViaProps>({
+  prop: 'gradientVia',
+  themeGet: getColor,
+  cssProperty: (_, { value }) => ({
+    '--x-gradient-stops': `var(--x-gradient-from), ${value}, var(--x-gradient-to, transparent)`,
+  }),
+})
+
+export interface GradientToProps<T = {}> {
+  gradientTo?: SystemProperty<ColorGetter, T>
+}
+export const gradientTo = style<GradientToProps>({
+  prop: 'gradientTo',
+  themeGet: getColor,
+  cssProperty: '--x-gradient-to',
+})
+
 export type BackgroundsProps<T = {}> = BackgroundProps<T> &
   BackgroundColorProps<T> &
   BackgroundImageProps<T> &
@@ -71,7 +123,10 @@ export type BackgroundsProps<T = {}> = BackgroundProps<T> &
   BackgroundPositionProps<T> &
   BackgroundRepeatProps<T> &
   BackgroundAttachmentProps<T> &
-  BackgroundClipProps<T>
+  BackgroundClipProps<T> &
+  GradientFromProps<T> &
+  GradientViaProps<T> &
+  GradientToProps<T>
 export const backgrounds = compose<BackgroundsProps>(
   background,
   backgroundColor,
@@ -81,4 +136,7 @@ export const backgrounds = compose<BackgroundsProps>(
   backgroundRepeat,
   backgroundAttachment,
   backgroundClip,
+  gradientFrom,
+  gradientVia,
+  gradientTo,
 )
