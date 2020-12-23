67d3262c16845a24949dc24033312d51f4c82f91
Greg Bergé <greg@smooth-code.com>
2020-12-23 09:57:30 +0100

    docs(website): colors & theme

--- website/src/smooth-doc/theme.js
+++ website/src/smooth-doc/theme.js
@@ -21,5 +21,14 @@ export const theme = deepmerge(baseTheme, {
     }
   `,
   space: [0, 4, 8, 16, 24, 48, 96, 144, 192, 240],
+  screens: {
+    _: 0,
+    xs: 0,
+    sm: 640,
+    md: 768,
+    lg: 1024,
+    xl: 1280,
+    '2xl': 1536,
+  },
   colors: { ...primaryColor('pink') },
 })
