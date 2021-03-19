0660565773898ec4f87eb66f90a42d5511753953
Greg Bergé <greg@smooth-code.com>
2021-03-19 09:03:44 +0100

    fix(types): invalid color prop

--- packages/styled-components/src/createX.ts
+++ packages/styled-components/src/createX.ts
@@ -6,13 +6,22 @@ type JSXElementKeys = keyof JSX.IntrinsicElements
 
 const tags = Object.keys(styled)
 
+type SafeIntrinsicComponent<T extends keyof JSX.IntrinsicElements> = (
+  props: Omit<JSX.IntrinsicElements[T], 'color'>,
+) => React.ReactElement<any, T>
+
 export const createX = <TProps extends object>(generator: StyleGenerator) => {
   type X<TProps extends object> = {
     extend<TExtendProps extends object>(
       ...generators: StyleGenerator[]
     ): X<TExtendProps>
   } & {
-    [Key in JSXElementKeys]: StyledComponent<Key, DefaultTheme, TProps, never>
+    [Key in JSXElementKeys]: StyledComponent<
+      SafeIntrinsicComponent<Key>,
+      DefaultTheme,
+      TProps,
+      'color'
+    >
   }
 
   // @ts-ignore
