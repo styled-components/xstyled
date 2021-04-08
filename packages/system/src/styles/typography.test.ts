08369145be722607f213c5b43f6b23263e7ca1a1
Greg Bergé <greg@smooth-code.com>
2021-04-09 00:07:21 +0200

    feat(text): new text utility
    Closes #178

--- packages/system/src/styles/typography.test.ts
+++ packages/system/src/styles/typography.test.ts
@@ -1,4 +1,4 @@
-import { getFontSize } from './typography'
+import { getFontSize, text } from './typography'
 
 describe('#getFontSize', () => {
   it('gets a value and transforms it according to spec.', () => {
@@ -15,5 +15,18 @@ describe('#getFontSize', () => {
     expect(getFontSize(2)(props)).toEqual('20px')
     expect(getFontSize(10)(props)).toEqual('10px')
     expect(getFontSize('50%')(props)).toEqual('50%')
+  })
+})
+
+describe('#text', () => {
+  it('gets value from the theme and transforms it', () => {
+    const theme = {
+      texts: { custom: { fontSize: 10, lineHeight: 1.5 } },
+    }
+    expect(text({ text: 'custom', theme })).toEqual({
+      fontSize: '10px',
+      lineHeight: 1.5,
+    })
+    expect(text({ text: 'xs', theme })).toEqual({})
   })
 })
