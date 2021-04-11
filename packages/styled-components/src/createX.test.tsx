afd5e4ac414c368a32cb64dd18435f45ea2668b1
Aron Griffis <aron@scampersand.com>
2021-04-11 15:25:09 -0400

    fix: x.* must pass through props for as={Component}
    This is from discussion at
    https://github.com/gregberge/xstyled/pull/231#issuecomment-817353699

--- packages/styled-components/src/createX.test.tsx
+++ packages/styled-components/src/createX.test.tsx
@@ -20,6 +20,40 @@ describe('#createX', () => {
       font-size: 10px;
     `)
   })
+
+  it('avoids passing system props to HTML element', () => {
+    const x = createX<FontSizeProps<Theme>>(fontSize)
+    const { container } = render(<x.div fontSize={10} />)
+    expect(container.firstChild).not.toHaveAttribute('fontSize')
+  })
+
+  it('passes HTML attrs to HTML element', () => {
+    const x = createX<FontSizeProps<Theme>>(fontSize)
+    const { container } = render(<x.div role="presentation" />)
+    expect(container.firstChild).toHaveAttribute('role', 'presentation')
+  })
+
+  it('avoids passing system props to "as" component', () => {
+    const Component = props => <div {...props} />
+    const x = createX<FontSizeProps<Theme>>(fontSize)
+    const { container } = render(<x.div as={Component} fontSize={10} />)
+    expect(container.firstChild).not.toHaveAttribute('fontSize')
+  })
+
+  it('passes non-system props to "as" component', () => {
+    const Component = ({asdf, ...props}) => <div {...props}>{asdf}</div>
+    const x = createX<FontSizeProps<Theme>>(fontSize)
+    const { container } = render(<x.div as={Component} asdf="boo!" />)
+    expect(container.firstChild).toHaveTextContent('boo!')
+  })
+
+  // skip because this depends on unreleased styled-components 5.2.4 or 6
+  it.skip('avoids passing non-HTML attrs to HTML element', () => {
+    const x = createX<FontSizeProps<Theme>>(fontSize)
+    const { container } = render(<x.div asdf="boo!" />)
+    expect(container.firstChild).not.toHaveAttribute('asdf')
+  })
+
 })
 
 describe('#x.extend', () => {
