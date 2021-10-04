af32c4395221371a8eb46f756fda3dfd9da61bf0
Evan Jacobs <ejacobs@aurorasolar.com>
2021-10-04 13:36:21 -0400

    fix: color prop missing responsive support

--- packages/system/src/styles/colors.ts
+++ packages/system/src/styles/colors.ts
@@ -1,12 +1,12 @@
 import * as CSS from 'csstype'
 import { themeGetter } from '../style'
-import { ThemeNamespaceValue, ITheme, Theme } from '../types'
+import { SystemProp, ThemeNamespaceValue, ITheme, Theme } from '../types'
 
 export type ThemeColor<T extends ITheme = Theme> = ThemeNamespaceValue<
   'colors',
   T
 >
-export type Color<T extends ITheme = Theme> = ThemeColor<T> | CSS.Property.Color
+export type Color<T extends ITheme = Theme> = SystemProp<ThemeColor<T> | CSS.Property.Color, T>;
 export const getColor = themeGetter<ThemeColor>({
   name: 'color',
   key: 'colors',
