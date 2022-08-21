df9a778c054145a5f58a0ce08bee6238d5fb5e8e
Igor Wessel <igor_wessel@hotmail.com>
2022-08-21 10:24:44 -0300

    tests(styled-components-native): fix settings

--- jest.config.js
+++ jest.config.js
@@ -1,13 +1,36 @@
 module.exports = {
-  testEnvironment: 'jsdom',
-  transformIgnorePatterns: ['<rootDir>.*(node_modules)(?!.*@xstyled.*).*$'],
-  transform: {
-    '^.+\\.(j|t)sx?$': 'babel-jest',
-  },
-  testPathIgnorePatterns: ['/node_modules/', '/website/'],
-  coveragePathIgnorePatterns: ['/node_modules/', '/dist/'],
-  moduleNameMapper: {
-    'styled-components':
-      '<rootDir>/node_modules/styled-components/dist/styled-components.browser.cjs.js',
-  },
+  projects: [
+    {
+      displayName: 'web',
+      testEnvironment: 'jsdom',
+      transformIgnorePatterns: ['<rootDir>.*(node_modules)(?!.*@xstyled.*).*$'],
+      transform: {
+        '^.+\\.(j|t)sx?$': 'babel-jest',
+      },
+      testPathIgnorePatterns: [
+        '/node_modules/',
+        '/website/',
+        '/packages/styled-components-native/',
+      ],
+      coveragePathIgnorePatterns: ['/node_modules/', '/dist/'],
+      moduleNameMapper: {
+        'styled-components':
+          '<rootDir>/node_modules/styled-components/dist/styled-components.browser.cjs.js',
+      },
+    },
+    {
+      displayName: 'native',
+      preset: 'react-native',
+      transformIgnorePatterns: ['node_modules/(?!@react-native|react-native)'],
+      transform: {
+        '^.+\\.(j|t)sx?$': 'babel-jest',
+      },
+      testPathIgnorePatterns: [
+        '/node_modules/',
+        '/website/',
+        '/packages/(?!styled-components-native)',
+      ],
+      coveragePathIgnorePatterns: ['/node_modules/', '/dist/'],
+    },
+  ],
 }
--- packages/styled-components-native/jest.config.cjs
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  preset: 'react-native',
-  transformIgnorePatterns: ['node_modules/(?!@react-native|react-native)'],
-  transform: {
-    '^.+\\.(j|t)sx?$': 'babel-jest',
-  },
-  testPathIgnorePatterns: ['/node_modules/', '/website/'],
-  coveragePathIgnorePatterns: ['/node_modules/', '/dist/'],
-}
