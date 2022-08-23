960edf2db54c3c6cacac76c657bfd760c404530e
Igor Wessel <igor_wessel@hotmail.com>
2022-08-23 19:33:36 -0300

    chore: remove package styled-components-native

--- packages/styled-components-native/src/scStyled.ts
+++
@@ -1,9 +0,0 @@
-import styled, {
-  DefaultTheme,
-  ReactNativeStyledInterface,
-} from 'styled-components/native'
-
-// Provide interop since `styled-components` does not work out of the box with ESM
-export const scStyled: ReactNativeStyledInterface<DefaultTheme> =
-  // @ts-ignore
-  typeof styled === 'function' ? styled : styled.default
