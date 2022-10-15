5d1b49f3a0d804d9a9d953ca8002f3bf6ea55a86
Greg Bergé <berge.greg@gmail.com>
2022-10-15 21:46:56 +0200

    docs: fix doc examples

--- packages/babel-preset-emotion-css-prop/src/index.test.ts
+++ packages/babel-preset-emotion-css-prop/src/index.test.ts
@@ -31,12 +31,9 @@ describe('preset', () => {
   it('transforms code', () => {
     expect(testPreset(code)).toMatchInlineSnapshot(`
       "function _extends() { _extends = Object.assign ? Object.assign.bind() : function (target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i]; for (var key in source) { if (Object.prototype.hasOwnProperty.call(source, key)) { target[key] = source[key]; } } } return target; }; return _extends.apply(this, arguments); }
-
       function _EMOTION_STRINGIFIED_CSS_ERROR__() { return "You have tried to stringify object returned from \`css\` function. It isn't supposed to be used directly (e.g. as value of the \`className\` prop), but rather handed to emotion so it can handle it (e.g. as value of \`css\` prop)."; }
-
       import * as React from 'react';
       import { jsx as ___xstyledEmotionJSX } from "@xstyled/emotion";
-
       var _ref = process.env.NODE_ENV === "production" ? {
         name: "3sn2xs",
         styles: "color:hotpink"
@@ -45,7 +42,6 @@ describe('preset', () => {
         styles: "color:hotpink;label:Button;",
         toString: _EMOTION_STRINGIFIED_CSS_ERROR__
       };
-
       export let Button = props => {
         return ___xstyledEmotionJSX(React.Fragment, null, ___xstyledEmotionJSX("button", _extends({
           css: _ref
