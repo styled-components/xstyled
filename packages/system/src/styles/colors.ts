63daead3def965df27ddc451a337f8bf6d675c86
Evan Jacobs <ejacobs@aurorasolar.com>
2023-03-16 17:36:37 -0400

    refactor: restore deep path generation for colors

--- packages/system/src/styles/colors.ts
+++ packages/system/src/styles/colors.ts
@@ -1,11 +1,8 @@
 import * as CSS from 'csstype'
 import { themeGetter } from '../style'
-import { ThemeNamespaceValue, ITheme, Theme } from '../types'
+import { SynthesizedPath, ITheme, Theme } from '../types'
 
-export type ThemeColor<T extends ITheme = Theme> = ThemeNamespaceValue<
-  'colors',
-  T
->
+export type ThemeColor<T extends ITheme = Theme> = SynthesizedPath<T['colors']>
 export type Color<T extends ITheme = Theme> = ThemeColor<T> | CSS.Property.Color
 export const getColor = themeGetter<ThemeColor>({
   name: 'color',
